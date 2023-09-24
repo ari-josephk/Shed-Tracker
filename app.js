67849c8649e78ffdd79bda446aeba57d0959a820
Ari <ari.khaytser@gmail.com>
2023-09-24 03:21:35 -0400

    Create timeseries API

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const fs = require('fs')
 const path = require('path')
+const csvParser = require('csv-parser')
+const cron = require('node-cron')
+
 const port = 3000
 
 app.get('/', (req, res) => {
@@ -12,6 +15,16 @@ app.use(express.static(path.resolve('public')))
 
 app.get('/csvData', (req, res) => {
 	res.sendFile(path.resolve('results.csv'))
+})
+
+app.get('/data', (req, res) => {
+	data = {}
+
+	fs.createReadStream('./results.csv')
+			.pipe(csvParser())
+			.on('headers', headers => headers.forEach(header => data[header] = new Array()))
+			.on('data', row => Object.keys(row).forEach(element => data[element].push(row[element])))
+			.on('end', () => res.send(data));
 })
 
 app.listen(port, () => {
